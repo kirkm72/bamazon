eac91d7ab2065a1cacde1e9de2608b2b8b9689cf
Kirk McGlamery <kirkm72@att.net>
2020-01-14 16:00:14 -0600

    initial product list working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,12 +5,20 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "password",
+    password: "",
     database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+    console.log("Connected as id# " + connection.threadId + "\n");
     displayItems();
 });
+
+function displayItems() {
+    connection.query("SELECT * FROM products", function (err, res) { // Selects all products
+        if (err) throw err;
+        console.table(res); // Built in table creator with response
+        //promptCustomer(res); // Uses inquirer to ask what to buy
+    });
+}
